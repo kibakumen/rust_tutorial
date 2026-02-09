c88191ca06a2902c3ba159857956539d83de993b
ST <shunyaterada@gmail.com>
2026-02-10 07:40:33 +0900

    feat:数当てゲーム完成！

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main(){
-    let x = 5;
-    let y = 10;
-
-    println!("x = {} and y = {}", x, y);
-
     println!("数字を予想してください！");
 
-    println!("予想を書き込もう！");
+    let secret_number = rand::rng().random_range(1..=100);
 
-    let mut guess = String::new();
+    loop {
+        println!("予想を書き込もう！");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("行の読み込みに失敗しました");
+        let mut guess = String::new();
 
-    println!("あなたの予想：{guess}");   
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("行の読み込みに失敗しました");
+
+        let guess: u32 = match guess.trim().parse(){
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("あなたの予想：{guess}");
+
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("小さすぎます"),
+            Ordering::Greater => println!("大きすぎます"),
+            Ordering::Equal => {
+                println!("あたりです！");
+                break;
+            },
+        }
+
+        println!("========================");  
+    }   
 }
 
